fb67e7701f73274393add9be4c154c7c5f22c2d5
Andrew McCourt <a.mccourt60@gmail.com>
2017-12-18 19:45:26 -0500

    testing api calls from server

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var express = require('express');
+var express    = require('express');
 var bodyParser = require('body-parser');
-var logger = require('morgan');
-var exphbs = require('express-handlebars');
-var _ = require('underscore');
+var logger     = require('morgan');
+var exphbs     = require('express-handlebars');
+var _          = require('underscore');
+var request    = require('request');
 
 var app = express();
 
@@ -14,10 +15,27 @@ app.set('view engine', 'handlebars');
 app.use('/css', express.static('css'));
 app.use('/js', express.static('js'));
 
-app.get('/', function(req, res) {
-    res.render('gameStats', {
-        error: 'unimplemented' 
-    });
+//  Temporary development API key needs to be changed every day
+var api_key = "RGAPI-ddcc7c3c-d0b0-4ff9-b5a0-0f2968cf122c";
+
+//  Standard URL declarations for organization purposes
+var base_url    = "https://na1.api.riotgames.com/lol/";
+var api_key_url = "?api_key=" + api_key;
+
+app.get('/:summoner_name', function(req, res) {
+
+	var summoner_name = req.params.summoner_name;
+	var summoner_url  = base_url + "summoner/v3/summoners/by-name/" + summoner_name + api_key_url;
+
+	request(summoner_url, function(error, response, body) {	
+		//var spectator_url = base_url + "spectator/v3/active-games/by-summoner/" + summoner_id + api_key_url;
+		console.log(JSON.stringify(body).id);
+		res.render('gameStats', {
+        	summoner: body["id"]
+    	});
+
+	});
+
 })
 
 app.listen(3000, function() {
